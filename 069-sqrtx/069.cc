dd374e3b2ea41f7f072ef3aabd20daa7c11544dc
Twopothead <3103204417@qq.com>
2019-07-04 22:29:41 +0800

    [AC] 069-sqrtx

--- 069-sqrtx/069.cc
+++ 069-sqrtx/069.cc
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 // 2147483647
+// testcase: 2147483647
+// runtime error: signed integer overflow: 46341 * 46341 cannot be represented in type 'int' (solution.c)
 int mySqrt(int x){
-    for(int i=1;i<=x/2;i++){
+    for(int i=0;i<=x;i++){
+        if(i>=46341)
+            return 46340;
         if(i*i>x)
             return i-1;
         if(i*i==x)
@@ -11,10 +15,13 @@ int mySqrt(int x){
     return 0;
 }
 int main(){
+    printf("%d ",mySqrt(1));   
     printf("%d ",mySqrt(4));   
     printf("%d ",mySqrt(8));   
     printf("%d ",mySqrt(16));   
     printf("%d ",mySqrt(121));   
     printf("%d ",mySqrt(122));   
+    printf("%d ",mySqrt(2147483647)); 
+    printf("%d ",mySqrt(2147395599));// 2147395599 46339
     return 0;
 }
