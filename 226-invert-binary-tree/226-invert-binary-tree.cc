aaf1823c2ea94c919e7dee08a43a746877b50e53
Twopothead <3103204417@qq.com>
2019-07-04 22:59:48 +0800

    [AC] 226-invert-binary-tree

---
+++ 226-invert-binary-tree/226-invert-binary-tree.cc
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    struct TreeNode *T;
+    T = root;
+    if(T){
+        struct TreeNode *t;
+        t = T->left;
+        T->left = T->right;
+        T->right = t;
+        invertTree(T->left);
+        invertTree(T->right);
+    }
+    return T;
+}
+int main(){
+    TreeNode *root;
+    // root = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->val = 4;
+    // root->left = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->right = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->left->val = 7;
+    // root->right->val = 2;
+    // root->left->left = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->left->right = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->left->left->val = 9;
+    // root->left->right->val = 6;
+    // root->left->left->left = NULL;
+    // root->left->left->right = NULL;
+    // root->left->right->left = NULL;
+    // root->left->right->right = NULL;
+    // root->right->left = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->right->right = (TreeNode*)malloc(sizeof(struct TreeNode));
+    // root->right->left->val = 3;
+    // root->right->right->val = 1;
+    // root->right->left->left = NULL;
+    // root->right->left->right = NULL;
+    // root->right->right->left = NULL;
+    // root->right->right->right = NULL;
+    invertTree(root);
+    return 0;
+}
+
