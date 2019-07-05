1f6b7d256ab213bdfc2795c3b2fa42f7d026b034
Twopothead <3103204417@qq.com>
2019-07-05 13:15:37 +0800

    [AC] 342-power-of-four

---
+++ 118/118.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> 
+// 1
+// 1    1
+// 1    2   1
+// 1    3   3   1
+// 1    4   6   4   1
+// 1    5   10  10  5   1 
+// 1    6   15  20  15  6   1
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+
+}
+int main(){
+    int *returnSize = (int*)malloc(sizeof(int));
+    
+    int **returnColumnSizes;
+    generate();
+    return 0;
+}
